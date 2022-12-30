465b7c3372a31c03c1f751032196be9ef2784fb4
DannyK18 <mbig4375@gmail.com>
2022-12-30 20:28:47 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Daniel on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
